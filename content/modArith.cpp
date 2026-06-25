54e5852df336966bae8075a4b33b7a11a226cd85
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-25 18:43:48 +0200

    Update modArith.cpp

--- content/modArith.cpp
+++ content/modArith.cpp
@@ -1,12 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-#define vi vector<int>
-#define pii pair<int, int>
-#define sz(x) (int)(x).size()
-
-
 /** Arithmétique modulaire
 **/
 
