d4ca963e2663cad3c7fb3ac4455459c433bd832f
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-25 18:11:54 +0200

    Update template.cpp

--- content/template.cpp
+++ content/template.cpp
@@ -5,3 +5,14 @@ using namespace std;
 #define vi vector<int>
 #define pii pair<int, int>
 #define sz(x) (int)(x).size()
+
+void run_task() {
+  // résoudre le problème ici
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  // int t; cin >> t; while(t--)
+  run_task();
+}
