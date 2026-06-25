85af6b5c9e32cbceeb96f921dbd29c2622a86629
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-25 18:24:54 +0200

    Update FenwickTree.cpp

--- content/FenwickTree.cpp
+++ content/FenwickTree.cpp
@@ -6,20 +6,22 @@ using namespace std;
 #define pii pair<int, int>
 #define sz(x) (int)(x).size()
 
-
+/** Fenwick Tree
+ * Dans les commentaires, le vecteur v représente le vecteur abstrait (0-indexed), et non le vecteur réel (1-indexed).
+**/
 struct FenwickTree {
 	vector<ll> v;
-	FenwickTree (int n) : v(n) {}
+	FenwickTree (int n) : v(n+1, 0ll) {}
 
 	// v[i] <- v[i] + x
 	void add(int i, ll x) {
-		for (int k = i+1; k <= sz(v); k += k&-k) v[k - 1] += x;
+		for (int k = i+1; k < sz(v); k += k&-k) v[k] += x;
 	}
 
 	// v[0] + .. + v[i-1]
 	ll sum(int i) {
 		ll s = 0;
-		for (int k = i; k > 0; k -= k&-k) s += v[k - 1];
+		for (int k = i; k > 0; k -= k&-k) s += v[k];
 		return s;
 	}
 };
