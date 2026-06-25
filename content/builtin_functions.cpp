42f0a91130d08f59d0ccdee8e57b2db19c7a0915
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-25 16:33:08 +0200

    Update builtin_functions.cpp

--- content/builtin_functions.cpp
+++ content/builtin_functions.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 /** Builtin functions
  * Fonctions utiles pour manipuler les entiers bit à bit
+ * ajouter ll à la fin pour passer un long long en argument (ex : __bultin_popcountll(x))
  * Quelques astuces : 
  * floor(log2(x)) = 31 - __builtin_clz(x),  si x > 0
  * v2(x) = __builtin_ctz(x&-x), si x > 0
@@ -18,10 +19,4 @@ int x;
 __builtin_popcount(x); // nb de bits égaux à 1
 __builtin_parity(x); // popcount mod 2
 __builtin_clz(x); // numbre de leading zeroes
-__builtin_ctz(x); // nombre de trailing zeroes,
-
-ll x;
-__builtin_popcountll(x);
-__builtin_parityll(x);
-__builtin_clzll(x);
-__builtin_ctzll(x);
+__builtin_ctz(x); // nombre de trailing zeroes
