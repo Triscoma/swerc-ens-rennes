0fb40abfbd3abd943a9c2a5436bbe09d0cc44d87
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-25 18:40:46 +0200

    Update modArith.cpp

--- content/modArith.cpp
+++ content/modArith.cpp
@@ -6,8 +6,10 @@ using namespace std;
 #define pii pair<int, int>
 #define sz(x) (int)(x).size()
 
+
 /** Arithmétique modulaire
 **/
+
 ll fast_exp_mod(ll a, ll e, int md) {
 	if (e == 0) return 1;
 	ll x = fast_exp_mod(a, e/2, md);
@@ -15,6 +17,7 @@ ll fast_exp_mod(ll a, ll e, int md) {
 	if (e % 2) x = (x * a) % md;
 	return x % md;
 }
+
 pii bezout(int a, int b) { // renvoie [u, v] tq a*u + b*v = pgcd(a, b)
 	if (b == 0) return {1, 0};
 	int q = a / b;
@@ -22,6 +25,7 @@ pii bezout(int a, int b) { // renvoie [u, v] tq a*u + b*v = pgcd(a, b)
 	auto [u, v] = bezout(b, r);
 	return {v, u - q * v};
 }
+
 int inv(int a, int b) { // renvoie x entre 0 et b-1 tq a*x = pgcd(a, b) [mod b]
 	int x = bezout(a, b).first;
 	return (x%b + b) % b;
