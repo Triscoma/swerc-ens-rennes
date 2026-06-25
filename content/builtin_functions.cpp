ee6d381c733dc9f81ebc62282409fe0c80875c69
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-25 18:43:23 +0200

    Update builtin_functions.cpp

--- content/builtin_functions.cpp
+++ content/builtin_functions.cpp
@@ -1,12 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-#define vi vector<int>
-#define pii pair<int, int>
-#define sz(x) (int)(x).size()
-
-
 /** Builtin functions
  * Fonctions utiles pour manipuler les entiers bit à bit
  * ajouter ll à la fin pour passer un long long en argument (ex : __bultin_popcountll(x))
