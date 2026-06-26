3f5b35193dc79c5fcabbc745abc3503f037c6d3e
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-26 10:58:05 +0200

    Update UnionFind.cpp

--- content/UnionFind.cpp
+++ content/UnionFind.cpp
@@ -7,14 +7,14 @@ struct UnionFind {
 
 	int size(int x) { return -e[find(x)]; }
 
-	int Find(int x) { 
+	int find(int x) { 
 		if (e[x] < 0) return x;
 		int p = find(e[x]);
 		e[x] = p;
 		return p;
 	}
 
-	bool Union(int a, int b) {
+	bool join(int a, int b) {
 		a = find(a); b = find(b);
 		if (a == b) return false;
 		if (e[a] > e[b]) swap(a, b);
