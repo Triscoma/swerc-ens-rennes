cb8bb654f2d979dbfa1c2f7db58e485b41f9efe0
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-29 17:05:56 +0200

    Update modArith.cpp

--- content/modArith.cpp
+++ content/modArith.cpp
@@ -1,24 +1,23 @@
 /** Arithmétique modulaire
- * Pour inv(a, b), si b est premier, faire fast_exp_mod(a, b-2, b) suffit.
 **/
-
-ll fast_exp_mod(ll a, ll e, int md) {
+ll mod_pow(ll a, ll e, int md) {
 	if (e == 0) return 1;
-	ll x = fast_exp_mod(a, e/2, md);
+	ll x = mod_pow(a, e/2, md);
 	x = (x * x) % md;
 	if (e % 2) x = (x * a) % md;
 	return x % md;
 }
-
-pii bezout(int a, int b) { // renvoie [u, v] tq a*u + b*v = pgcd(a, b)
+// renvoie [u, v] tq a*u + b*v = pgcd(a, b)
+pair<ll, ll> bezout(ll a, ll b) {
 	if (b == 0) return {1, 0};
-	int q = a / b;
-	int r = a % b;
+	ll q = a / b;
+	ll r = a % b;
 	auto [u, v] = bezout(b, r);
 	return {v, u - q * v};
 }
-
-int inv(int a, int b) { // renvoie x entre 0 et b-1 tq a*x = pgcd(a, b) [mod b]
-	int x = bezout(a, b).first;
+// renvoie x entre 0 et b-1 tq a*x = pgcd(a, b) [mod b]
+// Si b est premier, simplement faire mod_pow(a, b-2, b);
+ll inv(ll a, ll b) { 
+	ll x = bezout(a, b).first;
 	return (x%b + b) % b;
 }
