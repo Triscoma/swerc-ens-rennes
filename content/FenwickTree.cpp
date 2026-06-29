abe69a5a56e334260bbf5e4aca8e95610cdd808b
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-29 17:12:17 +0200

    Update FenwickTree.cpp

--- content/FenwickTree.cpp
+++ content/FenwickTree.cpp
@@ -1,16 +1,13 @@
 /** Fenwick Tree
  * Dans les commentaires, le vecteur v représente le vecteur abstrait (0-indexed), et non le vecteur réel (1-indexed).
 **/
-
 struct FenwickTree {
 	vector<ll> v;
 	FenwickTree (int n) : v(n+1, 0ll) {}
-
 	// v[i] <- v[i] + x
 	void add(int i, ll x) {
 		for (int k = i+1; k < sz(v); k += k&-k) v[k] += x;
 	}
-
 	// v[0] + .. + v[i-1]
 	ll sum(int i) {
 		ll s = 0;
