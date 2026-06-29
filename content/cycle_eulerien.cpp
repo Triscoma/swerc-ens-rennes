ea80069c4843b96167e994e6dacd2378d73e06f5
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-06-29 20:05:18 +0200

    Update cycle_eulerien.cpp

--- content/cycle_eulerien.cpp
+++ content/cycle_eulerien.cpp
@@ -1,12 +1,16 @@
-/** Cycle Eulerien
- * n = nombre de sommets de g, m = nombre d'arrêtes de g, g = graphe, s = sommet de départ
+/** Parcours Eulerien
+ * n = nombre de sommets de g, m = nombre d'arrêtes de g, g = graphe MIROIR (g lui-même si non-orienté), s = sommet de départ
  * g[i] = vecteur de couples {voisin, id_arete}
- * Hypothèse : pour tout i appartenant à la composante connexe de s (TODO : à définir pour les graphes orientés), g[i] est de taille paire (sinon il n'existe pas de cycle eulérien)
- * Renvoie un cycle eulérien de la composante connexe de s sous forme de vecteur (le premier et le dernier éléments sont égaux)
+ * Hypothèses : 
+ *    - Soit pour tout i appartenant à la composante connexe de s, g[i] est de taille paire
+ *    - Soit s et exactement 1 autre sommet t de la composante connexe de s ont un degré impair
+ *    --> Si g est orienté, chaque sommet doit avoir un degré nul, à l'exception éventuelle de s et t dont les degrés vallent respectivement -1 et 1
+ * Dans le premier cas, renvoie un cycle eulérien de la composante connexe de s (le premier et le dernier éléments sont égaux)
+ * Dans le second cas, renvoie un chemin eulérien TERMINANT en s
  * Si g n'est pas connexe, la taille de la sortie est différente de m+1
- * TODO : adapter pour qu'on puisse aussi renvoyer des chemins eulériens
+ * /!\ NON TESTE pour les chemins ni pour les graphes orientés
 **/
-vi euler_tour(int n, int m, vector<vector<pii>> g, int s) {
+vi euler_walk(int n, int m, vector<vector<pii>> g, int s) {
 	vi tour;
 	vector<bool> seen(m);
 	function<void(int)> dfs = [&](int u) {
