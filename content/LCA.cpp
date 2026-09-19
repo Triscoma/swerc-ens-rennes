d490f38f3023dfcc7eacb66fb4f4e1545132908d
Tristan Lambert <tristan.lbrt09@gmail.com>
2026-09-19 19:07:12 +0200

    Update LCA.cpp

--- content/LCA.cpp
+++ content/LCA.cpp
@@ -9,16 +9,16 @@ struct LCA {
     vi tin, inv;
     vector<vi> t;
 
-	  // par : vecteur des parents directs, par[root] = root
+  	// par : vecteur des parents directs, par[root] = root
     LCA(const vi& par) : n(sz(par)), tin(n), inv(n) {
         vector<vi> g(n);
-        int root = 0;
+        int root;
         for (int i = 0; i < n; i++) {
             if (par[i] == i) root = i;
             else g[par[i]].push_back(i);
-        }
+		}
         // tin = ordre préfixe, vérifie, en notant A[i] = tin[par[inv[i]]], 
-		    // tin[lca(x, y)] = min { A[i] | tin[x] < i <= tin[y] }
+		// tin[lca(x, y)] = min { A[i] | tin[x] < i <= tin[y] }
         int c = 0;
         vi st = {root};
         while (!st.empty()) {
@@ -26,16 +26,16 @@ struct LCA {
             tin[u] = c; inv[c++] = u;
             for (int v : g[u]) st.push_back(v);
         }
-		    // t[k][i] = min(A[i], ..., A[i + 2^k - 1])
+		// t[k][i] = min(A[i], ..., A[i + 2^k - 1])
         int LOG = 32 - __builtin_clz(n);
         t.assign(LOG, vi(n));
         for (int v = 0; v < n; v++) t[0][tin[v]] = tin[par[v]];
         for (int k = 1; k < LOG; k++) {
             for (int i = 0; i + (1 << k) <= n; i++) {
                 t[k][i] = min(t[k-1][i], t[k-1][i + (1 << (k-1))]);
-			      }
-		    }
-    }
+		  	}
+		}
+	}
 
     int get(int x, int y) {
         if (x == y) return x;
